9b34db1a1c033dda157708898ce782ffb8e79708
Camilo Parra <caparra92@gmail.com>
2020-10-06 20:04:59 -0500

    Updating config file

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,19 @@
 //    Puerto
 //=====================
 process.env.PORT = process.env.PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//=====================
+//    Base de datos
+//=====================
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://admin:tHGr2Aq6jMfEtum@cluster0.cpalr.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
+
