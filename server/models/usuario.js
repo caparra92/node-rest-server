85abf437fc82e9ce12fb39986a56c888a8ce769d
Camilo Parra <caparra92@gmail.com>
2020-10-06 19:05:13 -0500

    Adding rest routes and crud methods

---
+++ server/models/usuario.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const unique = require('mongoose-unique-validator');
+
+let Schema = mongoose.Schema;
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+}
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es requerido']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El email es requerido']
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es requerido']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        enum: rolesValidos,
+        default: 'USER_ROLE'
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
+usuarioSchema.plugin( unique, { message: '{PATH} debe de ser unico' } );
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 require('./config/config');
 
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
-
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -11,40 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
- 
-app.get('/', function (req, res) {
-  res.json('get usuario')
-});
 
-app.post('/usuario', function (req, res) {
+app.use(require('./routes/usuario'));
 
-    let body = req.body;
+mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true},(err, res)=> {
+  if (err) throw err;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            error: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            body
-        })
-    }
-    
-  });
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-  });
-
-app.delete('/', function (req, res) {
-    res.json('Delete usuario')
-  });
+  console.log('Base de datos ONLINE');
+})
  
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto', process.env.PORT );
